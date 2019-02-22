bf7f3aa47cad29d36413f44f2d768932688a61e5
ktlichkid <liqingsheng@pku.edu.cn>
2019-02-22 20:43:25 +0900

    Scripts to run all the

--- run_5.sh
+++ run_5.sh
@@ -1,6 +1,29 @@
-python3 plan-ddpg.py --game Ant --evalout Ant/01_evaluation --trainout Ant/01_train
-python3 plan-ddpg.py --game Walker2d --evalout Walker2d/01_evaluation --trainout Walker2d/01_train
-python3 plan-ddpg.py --game Hopper --evalout Hopper/01_evaluation --trainout Hopper/01_train
-python3 plan-ddpg.py --game HalfCheetah --evalout HalfCheetah/01_evaluation --trainout HalfCheetah/01_train
-python3 plan-ddpg.py --game Reacher --evalout Reacher/01_evaluation --trainout Reacher/01_train
+#!/bin/bash
 
+env CUDA_VISIBLE_DEVICES=$2 python3 plan-ddpg.py --game Ant --evalout Ant/$1_evaluation --trainout Ant/$1_train &
+
+env CUDA_VISIBLE_DEVICES=$2 python3 plan-ddpg.py --game Walker2d --evalout Walker2d/$1_evaluation --trainout Walker2d/$1_train &
+
+env CUDA_VISIBLE_DEVICES=$2 python3 plan-ddpg.py --game Hopper --evalout Hopper/$1_evaluation --trainout Hopper/$1_train &
+
+env CUDA_VISIBLE_DEVICES=$2 python3 plan-ddpg.py --game HalfCheetah --evalout HalfCheetah/$1_evaluation --trainout HalfCheetah/$1_train &
+
+env CUDA_VISIBLE_DEVICES=$2 python3 plan-ddpg.py --game Reacher --evalout Reacher/$1_evaluation --trainout Reacher/$1_train &
+
+env CUDA_VISIBLE_DEVICES=$2 python3 plan-ddpg.py --game AtlasForwardWalk --evalout AtlasForwardWalk/$1_evaluation --trainout AtlasForwardWalk/$1_train &
+
+wait
+
+env CUDA_VISIBLE_DEVICES=$2 python3 plan-ddpg.py --game Pong --evalout Pong/$1_evaluation --trainout Pong/$1_train &
+
+env CUDA_VISIBLE_DEVICES=$2 python3 plan-ddpg.py --game InvertedPendulum --evalout InvertedPendulum/$1_evaluation --trainout InvertedPendulum/$1_train &
+
+env CUDA_VISIBLE_DEVICES=$2 python3 plan-ddpg.py --game HumanoidFlagrun --evalout HumanoidFlagrun/$1_evaluation --trainout HumanoidFlagrun/$1_train &
+
+env CUDA_VISIBLE_DEVICES=$2 python3 plan-ddpg.py --game InvertedDoublePendulum --evalout InvertedDoublePendulum/$1_evaluation --trainout InvertedDoublePendulum/$1_train &
+
+env CUDA_VISIBLE_DEVICES=$2 python3 plan-ddpg.py --game HalfCheetah --evalout HalfCheetah/$1_evaluation --trainout HalfCheetah/$1_train &
+
+env CUDA_VISIBLE_DEVICES=$2 python3 plan-ddpg.py --game HumanoidFlagrunHarder --evalout HumanoidFlagrunHarder/$1_evaluation --trainout HumanoidFlagrunHarder/$1_train &
+
+wait
